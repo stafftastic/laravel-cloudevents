81f1d2e57271d4a903aa436d2bfe967606f3bde9
Hendrik Heil <hendrik@matters.dev>
2022-05-13 09:16:27 +0200

    refactor: rewrite

--- config/cloudevents.php
+++ config/cloudevents.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'default_source' => env('LARAVEL_CLOUDEVENTS_DEFAULT_SOURCE'),
     'endpoint' => env('LARAVEL_CLOUDEVENTS_DAPR_ENDPOINT', 'http://localhost:3500'),
     'pubsub_name' => env('LARAVEL_CLOUDEVENTS_DAPR_PUBSUB_NAME'),
 ];
--- src/CloudEventable.php
+++ src/CloudEventable.php
@@ -7,8 +7,5 @@ use CloudEvents\V1\CloudEventInterface;
 interface CloudEventable
 {
     public function toCloudEvent(): CloudEventInterface;
-    public function toCloudEventData(): array;
-    public function getCloudEventType(): string;
-    public function getCloudEventSource(): string;
     public function getCloudEventTopic(): string;
 }
