e6763a09bfd8df7974dcf41c6fc4a1fb56a1021a
Cristian Calara <cristian@derikon.com>
2022-08-25 14:54:10 +0300

    feedback updates

--- config/cloudevents.php
+++ config/cloudevents.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'brokers' => env('LARAVEL_CLOUDEVENTS_KAFKA_BROKERS', 'http://localhost:3500'),
+    'brokers' => env('LARAVEL_CLOUDEVENTS_KAFKA_BROKERS', 'kafka-broker:9092'),
     'enabled' => env('LARAVEL_CLOUDEVENTS_ENABLED', true),
     'debug_enabled' => env('LARAVEL_CLOUDEVENTS_DEBUG_ENABLED', false)
 ];
