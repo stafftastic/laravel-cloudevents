a7177291b04fb8df5d630ccf394f53931d5b11a8
Cristian Calara <cristian@derikon.com>
2022-08-25 14:27:20 +0300

    replace dapr with kafka

--- config/cloudevents.php
+++ config/cloudevents.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'endpoint' => env('LARAVEL_CLOUDEVENTS_DAPR_ENDPOINT', 'http://localhost:3500'),
-    'pubsub_name' => env('LARAVEL_CLOUDEVENTS_DAPR_PUBSUB_NAME'),
-    'enabled' => env('LARAVEL_CLOUDEVENTS_ENABLED', true)
+    'brokers' => env('LARAVEL_CLOUDEVENTS_KAFKA_BROKERS', 'http://localhost:3500'),
+    'enabled' => env('LARAVEL_CLOUDEVENTS_ENABLED', true),
+    'debug_enabled' => env('LARAVEL_CLOUDEVENTS_DEBUG_ENABLED', false)
 ];
